f787e299bb5968e4113e19505b17048d1aaaa2c8
eroe <cxz7852@naver.com>
2022-03-25 11:21:22 +0900

    first

--- app/src/main/java/com/example/eroe/MainActivity.java
+++ app/src/main/java/com/example/eroe/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //안녕하세요~ 김종원
+        //안녕하세요~ 김종원입니다.
         //송지호
     }
 }
