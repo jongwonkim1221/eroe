e48892b1b3c053b53555ba85810ab49aa519e6cb
jiuk5 <hss06201@naver.com>
2022-04-06 15:46:53 +0900

    login page

---
+++ app/src/androidTest/java/login_page.java
@@ -0,0 +1,15 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class login_page extends AppCompatActivity {
+
+    ImageView
+}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+}
