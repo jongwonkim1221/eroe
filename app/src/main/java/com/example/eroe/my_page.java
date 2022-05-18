2ec91a801d35bda0c9ec9a3ea734da461c9cfbc6
jiuk5 <hss06201@naver.com>
2022-05-18 16:00:40 +0900

    my_page.java

--- app/src/main/java/com/example/eroe/my_page.java
+++ app/src/main/java/com/example/eroe/my_page.java
@@ -1,4 +1,66 @@
 package com.example.eroe;
 
-public class my_page {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class my_page extends AppCompatActivity {
+
+    ImageButton logo_bt, notice_bt, profile_bt, menu_bt, photo_bt;
+    Button modify_bt, logout_bt;
+    TextView parent_txt;
+    View.OnClickListener cl;
+    Intent i, j, k, l;
+
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.my_page);
+
+        logo_bt = (ImageButton) findViewById(R.id.logo_bt);
+        notice_bt = (ImageButton) findViewById(R.id.notice_bt);
+        profile_bt = (ImageButton) findViewById(R.id.profile_bt);
+        menu_bt = (ImageButton) findViewById(R.id.menu_bt);
+        photo_bt = (ImageButton) findViewById(R.id.photo_bt);
+        modify_bt = (Button) findViewById(R.id.modify_bt);
+        logout_bt = (Button) findViewById(R.id.logout_bt);
+        parent_txt = (TextView) findViewById(R.id.parent_txt);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.logo_bt:
+                        i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                        break;
+                    case R.id.notice_bt:
+                        j = new Intent(getApplicationContext(), notice_page.class);
+                        startActivity(j);
+                        break;
+                    case R.id.profile_bt:
+                        k = new Intent(getApplicationContext(), my_page.class);
+                        startActivity(k);
+                        break;
+                    case R.id.menu_bt:
+                        l = new Intent(getApplicationContext(), menu_page.class);
+                        startActivity(l);
+                        break;
+                }
+            }
+        };
+        logo_bt.setOnClickListener(cl);
+        notice_bt.setOnClickListener(cl);
+        profile_bt.setOnClickListener(cl);
+        menu_bt.setOnClickListener(cl);
+        modify_bt.setOnClickListener(cl);
+        logout_bt.setOnClickListener(cl);
+    }
 }
