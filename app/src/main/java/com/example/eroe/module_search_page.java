40fd93ae9c53f623672ede53eaf8ced13c8723ce
jiuk5 <hss06201@naver.com>
2022-05-20 12:14:56 +0900

    my_info_modify.java service_center_page.java my_page.java module_search_page.java user_add_page.java

--- app/src/main/java/com/example/eroe/module_search_page.java
+++ app/src/main/java/com/example/eroe/module_search_page.java
@@ -1,4 +1,69 @@
 package com.example.eroe;
 
-public class module_search_page {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class module_search_page extends AppCompatActivity {
+
+    ImageButton logo_bt, notice_bt, profile_bt, menu_bt;
+    Button finish_bt;
+    TextView search_txt;
+    View.OnClickListener cl;
+    Intent i, j, k, l, m;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.my_page);
+
+        logo_bt = (ImageButton) findViewById(R.id.logo_bt);
+        notice_bt = (ImageButton) findViewById(R.id.notice_bt);
+        profile_bt = (ImageButton) findViewById(R.id.profile_bt);
+        menu_bt = (ImageButton) findViewById(R.id.menu_bt);
+        finish_bt = (Button) findViewById(R.id.finish_bt);
+        search_txt = (TextView) findViewById(R.id.search_txt);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.logo_bt:
+                        i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                        break;
+                    case R.id.notice_bt:
+                        j = new Intent(getApplicationContext(), notice_page.class);
+                        startActivity(j);
+                        break;
+                    case R.id.profile_bt:
+                        k = new Intent(getApplicationContext(), my_page.class);
+                        startActivity(k);
+                        break;
+                    case R.id.menu_bt:
+                        l = new Intent(getApplicationContext(), menu_page.class);
+                        startActivity(l);
+                        break;
+                    case R.id.finish_bt:
+                        m = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(m);
+                        Toast.makeText(getApplicationContext(), "검색이 완료 되었습니다.", Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+            }
+        };
+        logo_bt.setOnClickListener(cl);
+        notice_bt.setOnClickListener(cl);
+        profile_bt.setOnClickListener(cl);
+        menu_bt.setOnClickListener(cl);
+        finish_bt.setOnClickListener(cl);
+    }
 }
