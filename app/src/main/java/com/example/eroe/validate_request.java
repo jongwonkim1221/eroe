061f9eaefa8e3db61de817b62045970a30dd028c
jiuk5 <hss06201@naver.com>
2022-06-03 10:33:37 +0900

    지욱

---
+++ app/src/main/java/com/example/eroe/validate_request.java
@@ -0,0 +1,27 @@
+package com.example.eroe;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class validate_request extends StringRequest {
+    //서버 url 설정(php파일 연동)
+    final static  private String URL="http://dbconnect.iptime.org/Register.php";
+    private Map<String,String> map;
+
+    public validate_request(String userID, Response.Listener<String>listener){
+        super(Request.Method.POST,URL,listener,null);
+
+        map=new HashMap<>();
+        map.put("userID",userID);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
